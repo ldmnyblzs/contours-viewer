227ac8fd771a00836bd977ad030a7b0f8278fa64
Balázs Ludmány <ludmany.balazs@cloud.bme.hu>
2020-03-17 11:48:31 +0100

    Add license

--- inputform.hpp
+++ inputform.hpp
@@ -1,3 +1,22 @@
+/*
+  Copyright 2019 Balázs Ludmány
+
+  This file is part of contours-viewer.
+
+  contours-viewer is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  contours-viewer is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with contours-viewer.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef INPUT_FORM_HPP
 #define INPUT_FORM_HPP
 
